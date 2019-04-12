7150af006da355a117ab151db496283dea6b7886
Felipe Carvalho <karvalho_182@hotmail.com>
2019-04-12 13:30:27 +0000

    Add Goodreads API

--- application.py
+++ application.py
@@ -1,12 +1,16 @@
 import os
+import requests
 
 from flask import Flask, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-app = Flask(__name__)
+#API Goodreads
+res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "wstERZo4jV9Or7FSG2sIQ", "isbns": "9781632168146"})
+print(res.json())
 
+app = Flask(__name__)
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
